4c4f811f31c6c8433715dcb1f6d404db4ebcc5b0
Anas Alhariri <147917604+AlhaririAnas@users.noreply.github.com>
2024-05-16 20:26:14 +0200

    added create_database()

---
+++ resources/metadata_reader.py
@@ -0,0 +1,28 @@
+from resources.generator import ImageGenerator
+from PIL import Image
+import sqlite3
+
+# Path to the directory containing the images
+directory_path = "D:\\BigData-Data\\data\\image_data"
+img_gen = ImageGenerator(directory_path).image_generator()
+
+def create_database():
+    """
+    Creates a SQLite database and a table if it does not exist.
+    """
+    conn = sqlite3.connect('image_metadata.db')  # Connect to the SQLite database
+    c = conn.cursor()
+    # SQL command to create a table if it does not exist
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS metadata (
+            id INTEGER PRIMARY KEY,
+            filename TEXT,
+            height INTEGER,
+            width INTEGER,
+            format TEXT,
+            mode TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
