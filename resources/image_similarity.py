233126b4b522f429ef45fd1c9a83526559870a60
Anas Alhariri <147917604+AlhaririAnas@users.noreply.github.com>
2024-06-07 22:57:45 +0200

    added similarity tests

--- resources/image_similarity.py
+++ resources/image_similarity.py
@@ -1,5 +1,4 @@
 from scipy.spatial import distance
-from sklearn.metrics import jaccard_score
 from sklearn.metrics.pairwise import cosine_similarity as cos_similarity
 
 def euclidean_distance(v1, v2):
@@ -10,9 +9,3 @@ def manhattan_distance(v1, v2):
 
 def cosine_similarity(v1, v2):
     return cos_similarity([v1], [v2])[0][0]
-
-def jaccard_similarity(v1, v2):
-    return jaccard_score(v1, v2, average='weighted')
-
-def hamming_distance(v1, v2):
-    return distance.hamming(v1, v2)
