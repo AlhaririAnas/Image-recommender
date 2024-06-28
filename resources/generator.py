9d66c52aafe93508339cf0db75e8a607b210b213
Anas Alhariri <147917604+AlhaririAnas@users.noreply.github.com>
2024-06-28 15:49:15 +0200

    skip processed_paths

--- resources/generator.py
+++ resources/generator.py
@@ -10,14 +10,22 @@ class ImageGenerator:
         - image_generator(): Generates images (paths) from the directory one by one.
         """
         self.directory = directory
+        self.processed_paths = set()
 
     def image_generator(self):
         for root, _, files in os.walk(self.directory):
             for file in files:
+                
                 if file.lower().endswith(('png', 'jpg', 'jpeg')):
                     file_path = os.path.join(root, file)
+                    
+                    if file_path in self.processed_paths:
+                        continue
+                     
                     try:
                         image = Image.open(file_path)
                         yield image
+                        self.processed_paths.add(file_path) 
+                        
                     except Exception as e:
                         print(f"Error opening image {file_path}: {e}")
