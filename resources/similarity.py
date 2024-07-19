940e5942db143d945fa36830cea074a0f18b0c1c
Jonah Gräfe <jonah.greafe@gmail.com>
2024-07-19 17:43:27 +0200

    added yolo similarity measure

--- resources/similarity.py
+++ resources/similarity.py
@@ -1,10 +1,12 @@
 from resources.embedding import inception_v3
+from resources.yolo import get_yolo_classes
 
 ### import other similarity measure methods
 
 
 def get_similarities(img, args):
     features = inception_v3(img, args.device)
+    yolo_classes = get_yolo_classes(img)
     ### other similarities
 
-    return [features]
+    return [features, yolo_classes]
